276ec61e8a01ed8255cec6bdec28e0f2d19b2f3f
Phy0HK <phyoheinko.phk1@gmail.com>
2024-05-29 09:56:11 +0630

    GG

---
+++ Server/server.cjs
@@ -0,0 +1,70 @@
+const http = require("node:http");
+const port = 8080;
+const fs = require("fs");
+const mime = require('mime-types');
+const HtmlPath = ["../FrontEnd/Pages/login.html","../FrontEnd/Pages/register.html"]
+const defaultResPath = "../FrontEnd";
+const server = new http.createServer((req,res)=>{
+    const url = req.url;
+    const ReqType = mime.lookup(url);
+    console.log(url);
+    console.log(ReqType);
+    if(url=="/"){
+        fs.readFile("../FrontEnd/Pages/index.html","utf-8",(err,data)=>{
+            if(err) throw err;
+            else{
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.end(data);
+            }
+        })
+    }else if(url.toLowerCase()==="/login" || url.toLowerCase()==="/login/"){
+        fs.readFile(HtmlPath[0],"utf-8",(err,data)=>{
+            if(err) throw err;
+            else{
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+            }
+        })
+    }
+    else if(url.toLowerCase()=="/register" || url.toLowerCase()==="/register/"){
+        fs.readFile(HtmlPath[1],"utf-8",(err,data)=>{
+            if(err) throw err;
+            else{
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(data);
+            res.end();
+            }
+        })
+    }
+    else if(url.toLowerCase()=="/favicon.ico"){
+        fs.readFile("../FrontEnd/Resources/favicon.ico","utf-8",(err,data)=>{
+            if(err) throw err;
+            else{
+            res.writeHead(200,{"Content-Type":ReqType});
+            res.write(data);
+            res.end();
+            }
+        })
+    }
+    else{
+        try{
+        fs.readFile(defaultResPath+url,"utf-8",(err,data)=>{
+            if(err) throw err;
+            else{
+            res.writeHead(200,{"Content-Type":ReqType});
+            res.write(data);
+            res.end();
+            }
+        })
+    }catch{
+        res.writeHead(404,{"Content-Type":"text/html"})
+        res.write("<h1 align='center'>!!404 Not Found!!</h1>")
+        res.end();
+    }
+    }
+
+})
+server.listen(port,()=>{
+    console.log(`Server Listen to http://localhost:${port}`);
+})
