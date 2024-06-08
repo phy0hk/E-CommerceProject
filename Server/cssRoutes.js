8636dde6705fb678b47070ff88e160a04e552ef5
phy0hk <phyoheinko.phk1@gmail.com>
2024-06-08 21:22:15 +0630

    Updates

--- Server/cssRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/LoginStyle.css',(req,res)=>{
-    res.sendFile(__dirname+'/Main/css/LoginStyle.css');
-})
-router.get('/RegisterStyle.css',(req,res)=>{
-    res.sendFile(__dirname+'/Main/css/RegisterStyle.css');
-})
-module.exports = router;
